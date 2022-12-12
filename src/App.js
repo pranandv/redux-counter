c4a2195bb360ca544f0e106d7a5650ca930aff92
Pranand Vathare <pranandv@gmail.com>
2022-12-12 09:19:25 +0530

    react-redux counter application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector,useDispatch } from 'react-redux'
 
 function App() {
+  const dispatch = useDispatch()
+  const counter = useSelector((state)=>state.counter)
+
+  const inc = ()=>{
+    dispatch({type : "INCREMENT" })
+  }
+  const dec = ()=>{
+    dispatch({type:"DECREMENT"})
+  }
+  const add = ()=>{
+    dispatch({type:"ADD",payload:10})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter using Redux</h1>
+      <h2>{counter}</h2>
+      <button onClick={inc}>Increment</button>
+      <button onClick={dec}>Decrement</button>
+      <button onClick={add}>Add by 10</button>
+  
     </div>
   );
 }
